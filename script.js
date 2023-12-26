520c1b883d1579ad23d12909962d0027546da6ce
RodrigoStefano <152039767+RodrigoStefano@users.noreply.github.com>
2023-12-25 22:37:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let numberOne = inputUser("Digite o primeiro numero:");
+let numberTwo = inputUser("Digite o segundo numero:");
+
+alert(`A soma dos dois números é: ${numberOne + numberTwo}`);
+alert(`A subtação dos dois números é: ${numberOne - numberTwo}`);
+alert(`A miltipicação dos dois números é: ${numberOne * numberTwo}`);
+alert(`A divisão dos dois números é: ${numberOne / numberTwo}`);
+alert(`o resto da divisão dos dois números é: ${numberOne % numberTwo}`);
+
+if((numberOne + numberTwo) % 2 === 0){
+    alert("O resultado da soma é um númeor par");
+}
+else{
+    alert("O resultado da soma não é um número par");
+}
+
+if(numberOne == numberTwo){
+    alert("Os númeors informados são iguais");
+}
+else{
+    alert("Os números informados são diferentes");
+}
+
+
+function inputUser(mensage){
+    let input = prompt(mensage);
+    while(isNaN(input)){
+        input = prompt("O valor informado não é um número. Tente novamente");
+    }
+    input = Number(input);
+    return input;
+}
